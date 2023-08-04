4fc9f1ac0325d40892039c503ee396bcea5da0ec
anttohha <gomelnetwork@gmail.com>
2023-08-04 08:02:27 +0300

    FINAL

---
+++ Final/djangoprod/output/viod/orderform.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import DateInput
+
+from .models import *
+class addformorderfly(forms.Form):
+    name = forms.CharField(required=True, label='ФИО')
+    phone = forms.CharField(required=True, label='номер телефона')
+    address = forms.CharField(required=True, label='адресс')
+    email = forms.CharField(required=True, label='e-mail доставки')
